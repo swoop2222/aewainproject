f2740eeabc05598666a553c92301741eb8e93c8c
Muhammad Usama <m.usamaloralaiz@gmail.com>
2022-07-03 21:47:37 +0500

    Initial commit

---
+++ android/app/src/main/java/com/example/aewainproj/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.aewainproj;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+void main()=>runApp(myApp());
+
+class myApp extends StatelessWidget {
+  const myApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
